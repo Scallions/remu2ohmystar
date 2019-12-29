7233add9c3f83729b4dcd52db0090c6485ce6d72
Scallions <scallions@ScallionsdeiMac-Pro.local>
2019-12-30 03:03:31 +0800

    add log

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from remu2ohmystar import convert
 
+DEBUG = True
+
+from loguru import logger
+import sys
+logger.remove()
+if DEBUG:
+    logger.add(sys.stdout,level="DEBUG")
+else:
+    logger.add(sys.stdout,level="INFO")
 
 remufp = "./data/remu.json"
 outfp = "out.json"
--- remu2ohmystar/__init__.py
+++ remu2ohmystar/__init__.py
@@ -1,6 +1,9 @@
 
 def convert(remufp,out,userid):
     import json
+    from loguru import logger
+
+    logger.info("Convert from {}", remufp)
     remu = open(remufp)
     remu_obj = json.load(remu)
     remu.close()
@@ -20,13 +23,17 @@ def convert(remufp,out,userid):
         return None
 
     for key in repos.keys():
+        logger.debug("Convert repo {}", key)
         for tag in repos[key]:
             tag = get_tag(tag)
+            logger.debug("Repo {} tag {}", key, tag)
             if tag in ohmystar_obj["data"].keys():
                 ohmystar_obj["data"][tag].append(int(key))
             else:
                 ohmystar_obj["data"][tag] = [int(key)]
 
     ohmystarout = open(out,"w")
+    logger.info("Output to {}", out)
     json.dump(ohmystar_obj,ohmystarout)
     ohmystarout.close()
+    logger.info("Covert end")
